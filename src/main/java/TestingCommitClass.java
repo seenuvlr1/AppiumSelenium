285b9f0dc42b8d5002a45b786d6039585b2e79b4
Srinivasan <seenuvlr1@gmail.com>
2018-09-26 12:17:34 +0530

    Update TestingCommitClass.java

--- src/main/java/TestingCommitClass.java
+++ src/main/java/TestingCommitClass.java
@@ -6,6 +6,11 @@ public class TestingCommitClass {
 	{
 		System.out.println("This is my test method");
 	}
+	
+	public void testmethod2()
+	{
+	   System.out.println("This is my test method2");
+	}
 	public static void main(String[] args) {
 
 	}
