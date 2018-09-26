04b6c22d9220cb348363298d67757ec2a0d1297a
seenuvlr1 <seenuvlr1@gmail.com>
2018-09-26 12:29:08 +0530

    This is my third commit

--- src/main/java/TestingCommitClass.java
+++ src/main/java/TestingCommitClass.java
@@ -2,16 +2,19 @@
 public class TestingCommitClass {
 
 	
-	public void testmethod()
+	public static void testmethod()
 	{
 		System.out.println("This is my test method");
 	}
 	
-	public void testmethod2()
+	public static void testmethod2()
 	{
 	   System.out.println("This is my test method2");
 	}
 	public static void main(String[] args) {
+		
+		testmethod();
+		testmethod2();
 
 	}
 
